10f1a787bd22522231e2fae1f29f99b58562da36
Ari Ingber <ariingber@gmail.com>
2017-03-08 16:44:21 -0500

    block mobile view added

---
+++ js/mobileCheck.js
@@ -0,0 +1,43 @@
+(function() {
+  // anything smaller than 529 should change
+    $('#mobileCard').hide();
+    var windowWidth = $(window).width();
+
+  // check one when first open window
+
+  if ( windowWidth < 529 ) {
+    $('#main_content').remove();
+    $('#mobileCard').show();
+  }
+
+  // Looks for window resize function
+  $( window ).resize(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 529) {
+      $('#main_content').hide();
+      $('#mobileCard').show();
+    } else {
+      $('#mobileCard').hide();
+      $('#main_content').show();
+    }
+  });
+
+
+  // $('.content_container').css( 'width', '600px' );
+  // $('.content').css( 'width', '90%' );
+  //
+  // var leadHeight = $('#lead').height();
+  // $('.slave').css( 'height', leadHeight );
+  //
+  //
+  // if ( $(window).width() < 1350 ) {
+  //   $('#wall').css( 'justify-content', 'space-around' );
+  //   $('.content_container').css( 'width', '100%' );
+  //   $('.content').css( 'width', '90%' );
+  // }
+  //
+  // // LOOKS FOR WINDOW SIZE TO DETERMINE FLEXBOX SIZE AND DIV SIZE
+
+  // // **************************************************************
+
+}());
